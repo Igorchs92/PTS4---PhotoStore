2a1ba1b7d8a6a09008f286e6a3bc893ef1fef1d1
Igorchs92 <i.chernomorets@student.fontys.nl>
2016-04-20 10:53:16 +0200

    added picture classes, worked on filesystem

---
+++ PhotoStore/src/shared/files/ModifiedPicture.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shared.files;
+
+import java.io.File;
+
+/**
+ *
+ * @author Igor
+ */
+public class ModifiedPicture {
+
+    private int id;
+
+    public ModifiedPicture(int id) {
+        this.id = id;
+    }
+
+}
