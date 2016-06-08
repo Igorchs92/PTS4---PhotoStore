4197b79aec4200a1075e817adb24fbce0ae58ec4
Chernomorets,Igor I.I. <i.chernomorets@student.fontys.nl>
2016-06-08 11:13:02 +0200

    added interface

--- PhotoStore/src/client/producer/ui/ProducerClientMainController.java
+++ PhotoStore/src/client/producer/ui/ProducerClientMainController.java
@@ -11,8 +11,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.StackedAreaChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 
 /**
  * FXML Controller class
@@ -22,9 +25,41 @@ import javafx.scene.control.Label;
 public class ProducerClientMainController implements Initializable {
 
     @FXML
-    private Button button;
+    private Label lblPhotographerAmount;
     @FXML
-    private Label label;
+    private Label lblUserAmount;
+    @FXML
+    private Label lblGroupAmount;
+    @FXML
+    private Label lblUIDAmount;
+    @FXML
+    private Label lblPictureAmount;
+    @FXML
+    private Label lblOrderAmount;
+    @FXML
+    private Label lblTotalIncome;
+    @FXML
+    private Label lblIncomeToday;
+    @FXML
+    private Label lblIncomeWeek;
+    @FXML
+    private Label lblIncomeMonth;
+    @FXML
+    private Label lblIncomeYear;
+    @FXML
+    private ProgressBar pbProgress2;
+    @FXML
+    private Button btRegisterPhotographer;
+    @FXML
+    private StackedAreaChart<?, ?> cIncomeFreq;
+    @FXML
+    private ProgressBar pbProgress1;
+    @FXML
+    private BarChart<?, ?> cPictureFreq;
+    @FXML
+    private ProgressBar pbProgress;
+    @FXML
+    private BarChart<?, ?> cPhotographerBestEarning;
 
     /**
      * Initializes the controller class.
